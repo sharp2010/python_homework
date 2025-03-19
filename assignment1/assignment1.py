4be524c8aef1c6301285850820a98aee6cfaa369
Tracy Sharp <tracysharp@Tracys-MacBook-Pro.local>
2025-03-18 23:37:50 -0400

    task 3

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -11,4 +11,36 @@ def greet(name):
 greet("Tracy")
 
 # task 3:
+def calc(val1, val2, operation="multiply"):
+    try:    
+        match operation:
+            case "add":
+                return val1 + val2
+            case "subtract":
+                return val1 - val2
+            case "multiply":
+                return val1 * val2
+            case "divide":
+                if val2 == 0:
+                    raise ZeroDivisionError  # raise will trigger an exception
+                return val1 / val2
+            case "modulo":
+                return val1 % val2
+            case "int_divide":
+                if val2 == 0:
+                    raise ZeroDivisionError 
+                return val1 // val2 # will round down
+            case "power":
+                return val1 ** val2 # will raise number to a power
+            case _:
+                return "Invalid operation"
+    except ZeroDivisionError: # except will tell the program what to do if an error occurs
+        return "You can't divide by 0!"
+    except TypeError:
+        return "You can't multiply those values!"
+    
 
+    # Task 4
+
+    def data_type_conversion():
+        
